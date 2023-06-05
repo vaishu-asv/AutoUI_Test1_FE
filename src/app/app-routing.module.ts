70deacfe41d33c43d5e99b784e34d3a7d2f74871
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-05 13:05:51 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'authentication', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)},
+
+  { path: 'modules', loadChildren: () => import('./modules/modules.module').then(m => m.ModulesModule)},
 ];
 
 @NgModule({

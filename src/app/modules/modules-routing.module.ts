fef79934f6a579ae4929d497e57e054fb7468ce9
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-07 14:32:24 +0530

    Initial commit

--- src/app/modules/modules-routing.module.ts
+++ src/app/modules/modules-routing.module.ts
@@ -1,9 +1,12 @@
+import { CreatescheduleComponent } from './createschedule/createschedule.component';
 import { AdvancedSearchResultComponent } from './advanced-search-result/advanced-search-result.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'advanced-search-result', component: AdvancedSearchResultComponent },
+
+  { path: 'createschedule', component: CreatescheduleComponent },
 ];
 
 @NgModule({

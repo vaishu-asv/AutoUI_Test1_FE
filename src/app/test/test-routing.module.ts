3560fc5dfaf3399d693bcce350c6eca05ce0a9ce
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-07 14:32:35 +0530

    Initial commit

---
+++ src/app/test/test-routing.module.ts
@@ -0,0 +1,13 @@
+import { BaseLineListFinalComponent } from './base-line-list-final/base-line-list-final.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'base-line-list-final', component: BaseLineListFinalComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TestRoutingModule { }

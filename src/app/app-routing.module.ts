7f76c4ece03c1814789260e8f72a8e13f368f757
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-07 14:31:58 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ const routes: Routes = [
   { path: 'modules', loadChildren: () => import('./modules/modules.module').then(m => m.ModulesModule)},
 
   { path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule)},
+
+  { path: 'test', loadChildren: () => import('./test/test.module').then(m => m.TestModule)},
 ];
 
 @NgModule({
